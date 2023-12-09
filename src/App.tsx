e7aaaf794a7635435485f25952bbee3d4213a6c8
Uday Goel <udaygoel2000@gmail.com>
2023-12-09 22:44:37 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordian from "./components/Accordian";
+
+const data = [
+  {
+    key: "first",
+    value: {
+      title: "How much is my case worth?",
+      content: "Default Example of Law Firn NewsLetter",
+    },
+  },
+  {
+    key: "second",
+    value: {
+      title: "What should I do right after car accidect",
+      content: "Default Example of Law Firn NewsLetter",
+    },
+  },
+  {
+    key: "third",
+    value: {
+      title: "How much is my case worth?",
+      content: "Default Example of Law Firn NewsLetter",
+    },
+  },
+  {
+    key: "fourth",
+    value: {
+      title: "Do I need a personal injury report?",
+      content:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer.",
+    },
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className="w-[20%]">
+        <Accordian
+          data={data}
+          labelColor={"bg-[black]"}
+          accordianColor={"bg-[green]"}
+          textColor={"text-[white]"}
+        />
+      </div>
     </div>
   );
 }
